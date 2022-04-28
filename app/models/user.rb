a44cb482e5fbe1c59920be6a3c1034b3b7868766
amyleblanc <amymccarthy2014@gmail.com>
2022-04-27 19:41:22 -0700

    update model to include validation for User params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
 end
